10b2ab01c0f8fa8458a82c18206affc3c4b613ed
Rarag <Pyatkin@yandex.ru>
2022-10-06 00:17:38 +1000

    Add homework 1-3

---
+++ HW1-3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out int num);
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное.");
+}
+else
+{
+    Console.WriteLine("Число нечетное.");
+}
