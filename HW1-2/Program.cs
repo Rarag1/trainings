b8ce6a494fd04c267fabdc41d2b476c05246059d
Rarag <Pyatkin@yandex.ru>
2022-10-06 00:09:09 +1000

    Add homework 1-2

---
+++ HW1-2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите по очереди три числа.");
+Console.Write("Введите первое число: ");
+int.TryParse(Console.ReadLine(), out int firstNum);
+Console.Write("Введите второе число: ");
+int.TryParse(Console.ReadLine(), out int secondNum);
+Console.Write("Введите третье число: ");
+int.TryParse(Console.ReadLine(), out int thirdNum);
+int max = firstNum;
+if (secondNum > max)
+{
+    max = secondNum;
+}
+if (thirdNum > max)
+{
+    max = thirdNum;
+}
+Console.WriteLine(max);
