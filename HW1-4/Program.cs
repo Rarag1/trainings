427456d8abc23b99373171eee682d879ec7186e9
Rarag <Pyatkin@yandex.ru>
2022-10-06 00:33:49 +1000

    Add homework 1-4

---
+++ HW1-4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out int N);
+int num = 1;
+while (num <= N)
+{
+    if (num % 2 == 0)
+    {
+        Console.Write($"{num} ");
+    }
+    num += 1;
+}
