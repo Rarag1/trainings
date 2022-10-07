b49ab785377de2edff58ba4c67cda7fe9cfca9cf
Rarag <Pyatkin@yandex.ru>
2022-10-07 17:51:50 +1000

    Add homework 2-3

---
+++ HW2-3/Program.cs
@@ -0,0 +1,10 @@
+﻿int dayOfTheYear = new Random().Next() % 366;
+// допустим дни кратные 6 - субботы, кратные 7 - воскресенья (выходные), 1 число -понедельник.
+if (dayOfTheYear % 6 == 0 || dayOfTheYear % 7 == 0)
+{
+    Console.WriteLine($"Сегодня {dayOfTheYear} день в году - Выходной");
+}
+else
+{
+    Console.WriteLine($"Сегодня {dayOfTheYear} день в году - Arbeiten!!!");
+}
