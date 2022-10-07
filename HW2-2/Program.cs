4d75c77b0d69a7a4dcbba35c06fc2dee4279431b
Rarag <Pyatkin@yandex.ru>
2022-10-07 17:24:22 +1000

    Add homework 2-2

---
+++ HW2-2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out int num);
+if (num / 100 == 0)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    while (num / 1000 != 0)
+    {
+        Console.WriteLine(num = num / 10);
+    }
+    Console.WriteLine($"Третья цифра: {num % 10}");
+}
