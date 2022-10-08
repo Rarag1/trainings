6b727ba4c86329cd48c22d4c514ed7c8a1904212
Rarag <Pyatkin@yandex.ru>
2022-10-08 15:46:20 +1000

    Add homework 3-1

---
+++ HW3-1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write($"Введите пятизначное число: ");
+int.TryParse(Console.ReadLine(), out int num);
+//Console.WriteLine($" {num / 10000}, {num % 10}, {num % 10000 / 1000}, {num % 100 / 10}");
+if ((num / 10000 == num % 10) && (num % 10000 / 1000 == num % 100 / 10))
+{
+    Console.WriteLine($"Число {num} - полиндром.");
+}
+else
+{
+    Console.WriteLine($"Число {num} - не полиндром.");
+}
