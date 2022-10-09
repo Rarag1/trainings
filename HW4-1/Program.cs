0adbd0944e07b2c732af5e68dbb306c773dfb3ba
Rarag <Pyatkin@yandex.ru>
2022-10-09 21:26:57 +1000

    Add homework 4-1

---
+++ HW4-1/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = new Random().Next() % 10;
+int degree = new Random().Next(1, 10);
+
+int result = Degrees(num, degree);
+Console.WriteLine($"Чило {num} в степени {degree} = {result}");
+
+int Degrees(int _num, int _degree)
+{
+    int _result = _num;
+    for (int i = 1; i < _degree; i++)
+        _result = _result * _num;
+    return _result;
+
+}
