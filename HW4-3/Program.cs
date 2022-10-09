7cbe649444b8bec40d82571677b8f8139f3648ed
Rarag <Pyatkin@yandex.ru>
2022-10-09 22:42:29 +1000

    Add homework 4-3

---
+++ HW4-3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write($"Введите число равное 8 =)): ");
+int.TryParse(Console.ReadLine(), out int num);
+
+CreateArray(num);
+
+int[] CreateArray(int _num)
+{
+    int[] _array = new int[_num];
+    for (int i = 0; i < _num; i++)
+    {
+        _array[i] = new Random().Next() % 100;
+        Console.Write($"{_array[i]} ");
+    }
+    return _array;
+}
