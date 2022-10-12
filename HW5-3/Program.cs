32dc23749242dd1810a8eb674d1149b1dc0314c2
Rarag <Pyatkin@yandex.ru>
2022-10-12 20:15:29 +1000

    Add homework 5-3

---
+++ HW5-3/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76 */
+
+int[] array1 = CreateAndPrintArray();
+int max = array1[0], min = array1[0];
+foreach (int i in array1)
+{
+    if (i > max)
+        max = i;
+    else if (i < min)
+        min = i;
+}
+Console.WriteLine($"Разница между максимальным и минимальным значением элементов массива равна: {max - min}");
+
+
+int[] CreateAndPrintArray()
+{
+    Console.Write($"Задайте длину массива: ");
+    int.TryParse(Console.ReadLine()!, out int _size);
+    int[] _array = new int[_size];
+    for (int i = 0; i < _size; i++)
+    {
+        _array[i] = new Random().Next() % 100;
+        Console.Write($"{_array[i]} ");
+    }
+    Console.WriteLine();
+    return _array;
+}
