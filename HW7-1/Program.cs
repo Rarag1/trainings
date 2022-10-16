e5a0767236a1400f9e0d6814f9dd00996bb3f1d8
Rarag <Pyatkin@yandex.ru>
2022-10-16 15:29:54 +1000

    Add homework 7-1

---
+++ HW7-1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+int m = 3, n = 4;
+double[,] array1 = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array1[i, j] = Math.Round(new Random().NextDouble() * 20 - 10, 2);
+        Console.Write($"{array1[i, j]}  ");
+    }
+    Console.WriteLine();
+}
