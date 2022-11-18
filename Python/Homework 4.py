34c9ebe7b9cd0470c67ddf1488223aa77bc223b8
Rarag <Pyatkin@yandex.ru>
2022-11-18 12:32:40 +1000

    making beauty

--- Python/Homework 4.py
+++ Python/Homework 4.py
@@ -3,37 +3,37 @@
 # при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
 # формула Саймона Плаффа
-# d = int(input("Задайте точность: "))
-# pi = 0
-# for k in range(d):
-#     pi += (((1/16)**k)*(4/(8*k+1)-2/(8*k+4)-1/(8*k+5)-1/(8*k+6)))
-# print(round(pi, d))
+d = int(input("Задайте точность: "))
+pi = 0
+for k in range(d):
+    pi += (((1/16)**k)*(4/(8*k+1)-2/(8*k+4)-1/(8*k+5)-1/(8*k+6)))
+print(round(pi, d))
 
 
 # Задайте натуральное число N. Напишите программу, 
 # которая составит список простых множителей числа N.
 
-# N = int(input("Введите число: "))
-# result =[]
-# mult = 2
-# while mult <= N:
-#     if N % mult == 0:
-#         result.append(mult)
-#         N /= mult
-#     else: mult += 1
-# print(result)
+N = int(input("Введите число: "))
+result =[]
+mult = 2
+while mult <= N:
+    if N % mult == 0:
+        result.append(mult)
+        N /= mult
+    else: mult += 1
+print(result)
 
 
 # Задайте последовательность чисел. Напишите программу, 
 # которая выведет список неповторяющихся элементов исходной последовательности.
 
-# list1 = list(map(int, input("Введите список: ").split()))
-# list2 = []
-# for e in list1:
-#     if e not in list2:
-#         list2.append(e)
-# print(list1)
-# print(list2)
+list1 = list(map(int, input("Введите список: ").split()))
+list2 = []
+for e in list1:
+    if e not in list2:
+        list2.append(e)
+print(list1)
+print(list2)
 
 
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов 
@@ -41,17 +41,17 @@
 # Пример:
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-# import random
-# k = int(input('Введите степень многочлена: '))
-# m = ""
-# for i in range(k):
-#     r = random.randint(1,10)
-#     if r !=0:
-#         m += str(r)+"x**"+str(k) + "+"
-#     k-=1
-# m += str(random.randint(0,100)) + "=0"
-# with open('Homwork4.txt', 'w') as HW:
-#     HW.writelines(m)
+import random
+k = int(input('Введите степень многочлена: '))
+m = ""
+for i in range(k):
+    r = random.randint(1,10)
+    if r !=0:
+        m += str(r)+"x**"+str(k) + "+"
+    k-=1
+m += str(random.randint(0,100)) + "=0"
+with open('Homwork4.txt', 'w') as HW:
+    HW.writelines(m)
 
 
 # Даны два файла, в каждом из которых находится запись многочлена. 
