c8aaea137be982ecb1d99f4f51f37236222921a2
Rarag1 <>
2022-12-02 13:59:46 +1000

    Modified TelegramBot

--- Python/TelegramBot/Game.py
+++ Python/TelegramBot/Game.py
@@ -3,6 +3,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import numpy as np
 from random import randint
 
+# global rang, lucky, A
+
 
 async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'Hello {update.effective_user.first_name}. Сиграем в крестики-нолики?')
@@ -10,38 +12,61 @@ async def game(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f'{A}')
 
     while True:
-        if A[0,:].sum() == 4 or A[1,:].sum() == 4 or A[2,:].sum() == 4 or A[3,:].sum() == 4 or\
-        A[:,0].sum() == 4 or A[:,1].sum() == 4 or A[:,2].sum() == 4 or A[:,3].sum() == 4 or\
-        np.diag(A).sum() == 4 or np.diag(-A).sum() == 4:
+        if A[0, :].sum() == 4 or A[1, :].sum() == 4 or A[2, :].sum() == 4 or A[3, :].sum() == 4 or\
+            A[:, 0].sum() == 4 or A[:, 1].sum() == 4 or A[:, 2].sum() == 4 or A[:, 3].sum() == 4 or\
+                np.diag(A).sum() == 4 or np.diag(-A).sum() == 4:
             await update.message.reply_text(f"Выиграл 1-й игрок.")
             break
-        elif A[0,:].sum() == 20 or A[1,:].sum() == 20 or A[2,:].sum() == 20 or A[3,:].sum() == 20 or\
-        A[:,0].sum() == 20 or A[:,1].sum() == 20 or A[:,2].sum() == 20 or A[:,3].sum() == 20 or\
-        np.diag(A).sum() == 20 or np.diag(-A).sum() == 20:
+        elif A[0, :].sum() == 20 or A[1, :].sum() == 20 or A[2, :].sum() == 20 or A[3, :].sum() == 20 or\
+            A[:, 0].sum() == 20 or A[:, 1].sum() == 20 or A[:, 2].sum() == 20 or A[:, 3].sum() == 20 or\
+                np.diag(A).sum() == 20 or np.diag(-A).sum() == 20:
             await update.message.reply_text(f"Выиграл 2-ой игрок.")
             break
 
-        rang = randint(1,2) # добавлен фактор рандомного призового хода
-        lucky = randint(0,1) # есть возможность перебивать занятую позицию, если удача с тобой
+        rang = randint(1, 2)  # добавлен фактор рандомного призового хода
+        # есть возможность перебивать занятую позицию, если удача с тобой
+        lucky = randint(0, 1)
         if rang == 1:
-            await update.message.reply_text(f"Ходит 1-й игрок. Введите 2-е координаты (от 0 до 3) хода через запятую:")
+            await update.message.reply_text(f"Ходит 1-й игрок. Введите /move и 2-е координаты (от 0 до 3) хода через пробел:")
             msg = update.message.text
-            coords = msg.split(",")
-            i = int(coords[0])
-            j = int(coords[1])
-            if A[i,j] == 0: A[i,j] = 1
-            elif A[i,j] != 0 and lucky !=0: A[i,j] = 1 
-            await update.message.reply_text(f'{A}')
-            await update.message.reply_text(f'lucky ={lucky}')
-            rang+=1
+            print(msg)
+            # coords = msg.split()
+            # i = int(coords[0])
+            # j = int(coords[1])
+            # if A[i, j] == 0:
+            #     A[i, j] = 1
+            # elif A[i, j] != 0 and lucky != 0:
+            #     A[i, j] = 1
+            # await update.message.reply_text(f'{A}')
+            # await update.message.reply_text(f'lucky ={lucky}')
+            # rang += 1
+            break
         else:
-            await update.message.reply_text(f"Ходит 2-ой игрок. Введите 2-е координаты (от 0 до 3) хода через запятую:")
+            await update.message.reply_text(f"Ходит 2-ой игрок. Введите команду /move и 2-е координаты (от 0 до 3) хода через пробел:")
             msg = update.message.text
-            coords = msg.split()
-            i = int(coords[0])
-            j = int(coords[1])
-            if A[i,j] == 0: A[i,j] = 5
-            elif A[i,j] != 0 and lucky !=0: A[i,j] = 5 
-            await update.message.reply_text(f'{A}')
-            await update.message.reply_text(f'lucky ={lucky}')
-            rang-=1
+            print(msg)
+            # coords = msg.split()
+            # i = int(coords[0])
+            # j = int(coords[1])
+            # if A[i, j] == 0:
+            #     A[i, j] = 5
+            # elif A[i, j] != 0 and lucky != 0:
+            #     A[i, j] = 5
+            # await update.message.reply_text(f'{A}')
+            # await update.message.reply_text(f'lucky ={lucky}')
+            # rang -= 1
+            break
+
+# async def move(update: Update, context: ContextTypes.DEFAULT_TYPE):
+#     await update.message.reply_text(f'Ход игрока.')
+#     msg = update.message.text
+#     coords = msg.split()
+#     i = int(coords[1])
+#     j = int(coords[2])
+#     if A[i, j] == 0:
+#         A[i, j] = 1
+#     elif A[i, j] != 0 and lucky != 0:
+#         A[i, j] = 1
+#     await update.message.reply_text(f'{A}')
+#     await update.message.reply_text(f'lucky ={lucky}')
+#     rang += 1
--- Python/TelegramBot/main.py
+++ Python/TelegramBot/main.py
@@ -1,12 +1,15 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import config
 from apps import *
 from Game import *
 
-app = ApplicationBuilder().token("5886826050:AAE05-_E_5k8mkGOC1SnFudRMkBVkEhsOwg").build()
+app = ApplicationBuilder().token(config.TOKEN).build()
 
 app.add_handler(CommandHandler("hello", hello))
 app.add_handler(CommandHandler("game", game))
+# app.add_handler(CommandHandler("move", move))
+
 
 print('Server start')
 app.run_polling()
